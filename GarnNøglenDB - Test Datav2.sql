b7b95c4d5f72fcab141d1711a023bcf337a54600
jensbrobak <jensbrobakn@gmail.com>
2018-02-01 14:53:18 +0100

    Tilføjet beskrivelse til begge uploads

--- GarnNøglenDB - Test Datav2.sql
+++ GarnNøglenDB - Test Datav2.sql
@@ -8,37 +8,28 @@ INSERT INTO `Persons` (`Id`, `firstName`, `lastName`, `Phone`, `Email`, `birthDa
 INSERT INTO `Persons` (`Id`, `firstName`, `lastName`, `Phone`, `Email`, `birthDate`) VALUES (NULL, 'Jens', 'Skrubbeltrang', '11', 'jens@gmail.com', '2012-10-05');
 
 
-INSERT INTO `Projects_Status`(`Id`, `status`) VALUES (NULL,'Åben');
+INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'1','trøje','blå','Åben','Bumbo','206','lyseblå','170','2','1003','blive færdig til maj','6');
 
 
-INSERT INTO `Projects_Status`(`Id`, `status`) VALUES (NULL,'Igangværende');
+INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'2','bukser','rød','Igangværende','Anna','22491','mørkerød','160','4','162065','Svært at lave','10');
 
 
-INSERT INTO `Projects_Status`(`Id`, `status`) VALUES (NULL,'Færdig');
+INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'3','hue','grøn','Færdig','Tykke Bertha','102','darkgreen','50','6','505','Ikke så ringe endda','100');
 
 
-INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status_Id`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'1','trøje','blå','1','Bumbo','206','lyseblå','170','2','1003','blive færdig til maj','6');
+INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`, `description`) VALUES (NULL,'1','Google Drive/path/10.png', 'billede1');
 
 
-INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status_Id`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'2','bukser','rød','2','Anna','22491','mørkerød','160','4','162065','Svært at lave','10');
+INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`, `description`) VALUES (NULL,'1','Google Drive/path/10.jpg', 'billede2');
 
 
-INSERT INTO `Projects`(`Id`, `person_Id`, `name`, `description`, `status_Id`, `yarnProductName`, `yarnColorCode`, `yarnColor`,`yarnLength`, `needleSize`, `batchNr`, `notes`, `counter`) VALUES (NULL,'3','hue','grøn','3','Tykke Bertha','102','darkgreen','50','6','505','Ikke så ringe endda','100');
+INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`, `description`) VALUES (NULL,'2','Google Drive/path/10.bitmap', NULL);
 
 
-INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`) VALUES (NULL,'1','Google Drive/path/10.png');
+INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`, `description`) VALUES (NULL,'1', NULL, NULL, 'http://www.garnopskrifter.dk/lyseblåtrøje.pdf', 'lyseblåtrøje');
 
 
-INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`) VALUES (NULL,'1','Google Drive/path/10.jpg');
+INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`, `description`) VALUES (NULL,'1', NULL, 'Alt om Hækling - side 3', NULL, 'trøje');
 
 
-INSERT INTO `Projects_PicturePaths`(`Id`, `project_Id`, `uploadPath`) VALUES (NULL,'2','Google Drive/path/10.bitmap');
-
-
-INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`) VALUES (NULL,'1', NULL, NULL, 'http://www.garnopskrifter.dk/lyseblåtrøje.pdf');
-
-
-INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`) VALUES (NULL,'1', NULL, 'Alt om Hækling - side 3', NULL);
-
-
-INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`) VALUES (NULL,'2','Google Drive/path/darkgreensweater.docx', NULL, NULL);
+INSERT INTO `Projects_RecipePaths`(`Id`, `project_Id`, `uploadPath`, `bookPage`, `link`, `description`) VALUES (NULL,'2','Google Drive/path/darkgreensweater.docx', NULL, NULL, NULL);
--- GarnNøglenDBv2.sql
+++ GarnNøglenDBv2.sql
@@ -9,13 +9,6 @@ CREATE TABLE Persons (
     UNIQUE (Id)
   );
 
-CREATE TABLE Projects_Status (
-    Id int NOT NULL AUTO_INCREMENT,
-    status varchar(255) NOT NULL,
-    PRIMARY KEY (Id),
-    UNIQUE (Id)
-  );
-
 CREATE TABLE Projects (
       Id int NOT NULL AUTO_INCREMENT,
       person_Id int NOT NULL,
@@ -40,6 +33,7 @@ CREATE TABLE Projects_PicturePaths (
       Id int NOT NULL AUTO_INCREMENT,
       project_Id int NOT NULL,
       uploadPath varchar(255),
+      description varchar(255),
       PRIMARY KEY (Id),
       UNIQUE (Id),
       FOREIGN KEY (project_Id) REFERENCES Projects(Id)
@@ -51,6 +45,7 @@ CREATE TABLE Projects_RecipePaths (
       uploadPath varchar(255),
       bookPage varchar(255),
       link varchar(255),
+      description varchar(255),
       PRIMARY KEY (Id),
       UNIQUE (Id),
       FOREIGN KEY (project_Id) REFERENCES Projects(Id)
