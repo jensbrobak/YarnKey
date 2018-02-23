a3a5e4bb96d2aa3d13ec590f719ef1bed0f3bcb8
jensbrobak <jensbrobakn@gmail.com>
2018-02-23 16:35:17 +0100

    Færdiggørelse af forrige opdatering

--- src/pages/projectedit/projectedit.ts
+++ src/pages/projectedit/projectedit.ts
@@ -3,13 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Toast } from '@ionic-native/toast';
 
-/**
- * Generated class for the ProjecteditPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-projectedit',
@@ -17,10 +10,7 @@ import { Toast } from '@ionic-native/toast';
 })
 export class ProjecteditPage {
 
-
-
   project = { rowid:0, name:"", description:"", status:"Igangværende",  yarnProductName:"",  yarnColorCode:"", yarnColor:"",  yarnLength:"",  needleSize:"",  batchNr:"", notes:"", counter:0 };
-
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -47,9 +37,8 @@ export class ProjecteditPage {
             this.project.yarnLength = res.rows.item(0).yarnLength;
             this.project.needleSize = res.rows.item(0).needleSize;
             this.project.batchNr = res.rows.item(0).batchNr;
+            this.project.notes = res.rows.item(0).notes;
             this.project.counter = res.rows.item(0).counter;
-            
-          
           }
         })
         .catch(e => {
@@ -75,7 +64,7 @@ export class ProjecteditPage {
       name: 'yarnkeydb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('UPDATE projects SET name=?, description=?, status=?, yarnProductName=?, yarnColorCode=?, yarnColor=?, yarnLength=?, needleSize=?, batchNr=?, notes=?, counter=? WHERE rowid=?',[this.project.name,this.project.description,this.project.status,this.project.yarnProductName,this.project.yarnColorCode,this.project.yarnColor,this.project.yarnLength,this.project.needleSize,this.project.batchNr,this.project.counter,this.project.rowid])
+      db.executeSql('UPDATE projects SET name=?, description=?, status=?, yarnProductName=?, yarnColorCode=?, yarnColor=?, yarnLength=?, needleSize=?, batchNr=?, notes=?, counter=? WHERE rowid=?',[this.project.name,this.project.description,this.project.status,this.project.yarnProductName,this.project.yarnColorCode,this.project.yarnColor,this.project.yarnLength,this.project.needleSize,this.project.batchNr,this.project.notes,this.project.counter,this.project.rowid])
       .then(res => {   
       console.log(res);
           this.toast.show('Projekt opdateret', '5000', 'center').subscribe(
